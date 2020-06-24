59fabe52ce331638141d0fbc0cc1b353bd6c17a9
geekxi <geekxi@outlook.com>
2020-06-24 13:06:15 +0800

    11

--- code/test.py
+++ code/test.py
@@ -16,10 +16,16 @@
 # print(j)
 
 
-for i in range(1, 10):
-    print("i开始值", i)
-    if i%2 == 0:
-        print("是偶数", i)
-        continue
-    i = i + 2
-    print("i的值为", i)
+# for i in range(1, 10):
+#     print("i开始值", i)
+#     if i%2 == 0:
+#         print("是偶数", i)
+#         continue
+#     i = i + 2
+#     print("i的值为", i)
+
+import pymysql
+
+db = pymysql.connect('192.168.8.74', 'root','root', 'pystudy')
+
+cursor = db.cursor()
